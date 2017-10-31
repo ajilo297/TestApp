b2d88bf19b015d426e91d0da14a25dd14ba2786c
Ajil Oommen <ajilo297@gmail.com>
2017-10-31 19:28:05 +0530

    Added star animation test

---
+++ app/src/main/java/ajil/com/testapp/ConstraintIssue.java
@@ -0,0 +1,13 @@
+package ajil.com.testapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ConstraintIssue extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_constraint_issue);
+    }
+}
