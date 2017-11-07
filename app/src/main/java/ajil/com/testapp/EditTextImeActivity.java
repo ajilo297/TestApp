b1685a9cde59702e5c210cc64e3d3524139d6e68
Ajil Oommen <ajilo297@gmail.com>
2017-11-07 15:01:49 +0530

    EditTextIme working

--- app/src/main/java/ajil/com/testapp/EditTextImeActivity.java
+++ app/src/main/java/ajil/com/testapp/EditTextImeActivity.java
@@ -2,6 +2,10 @@ package ajil.com.testapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.InputType;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class EditTextImeActivity extends AppCompatActivity {
 
@@ -9,5 +13,10 @@ public class EditTextImeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_text_ime);
+
+        EditText editText = findViewById(R.id.textView);
+
+        editText.setImeOptions(EditorInfo.IME_ACTION_SEND);
+        editText.setRawInputType(InputType.TYPE_CLASS_TEXT);
     }
 }
